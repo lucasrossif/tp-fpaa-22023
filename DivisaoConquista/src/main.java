f5ce2f553069d202c001ed3029e1c3f9113f71a6
Rafael-Ferraz-Barra <104329840+Rafael-Ferraz-Barra@users.noreply.github.com>
2023-12-12 21:44:32 -0300

    Add files via upload

---
+++ DivisaoConquista/src/main.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class TesteDivEConq {
+
+    public static void main(String[] args) {
+        int numConjuntos = 10;
+        int numCaminhoes = 3;
+        int numRotas = 25;
+
+        long tempoInicial = System.nanoTime();
+
+        for (int i = 0; i < numConjuntos; i++) {
+            List<int[]> conjuntoDeRotas = GeradorDeProblemas.geracaoDeRotas(numRotas, 1, 0.5);
+            int[] rotas = conjuntoDeRotas.get(0);
+
+            List<List<Integer>> distribuicao = DivEConq.distribuirRotas(rotas, numCaminhoes);
+
+            for (int j = 0; j < distribuicao.size(); j++) {
+                System.out.println("Caminhão " + (j + 1) + ": rotas " + distribuicao.get(j) +
+                        " - total " + DivEConq.calcularQuilometragem(distribuicao.get(j)) + "km");
+            }
+
+            System.out.println("---------------");
+        }
+
+        long tempoFinal = System.nanoTime();
+        long tempoTotal = tempoFinal - tempoInicial;
+
+        System.out.println("Tempo total de execução: " + tempoTotal + " nanossegundos");
+    }
+}
