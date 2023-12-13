4d0dd34848766798dc665fa000354875bb7418c1
SitaGomes <ansitagomes@gmail.com>
2023-12-12 21:02:19 -0300

    feat: track event for backtraking

--- backtracking/src/Backtraking.java
+++ backtracking/src/Backtraking.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Backtraking {
@@ -6,26 +7,49 @@ class Backtraking {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        for (int i = 0; i < 10; i++) {
-            List<int[]> conjuntoDeTeste = GeradorDeProblemas.geracaoDeRotas(26, 1, 1);
-            System.out.println("------- Conjunto de teste " + i + " -------");
-            for (int[] rotas : conjuntoDeTeste) {
-                int meta = 0;
-                for (int rota : rotas) {
-                    meta += rota;
-                }
-                List<List<Number>> caminhoes = backtracking(rotas, new int[3], new ArrayList<>(), meta / 3, 0);
-                for (List<Number> caminhao : caminhoes) {
-                    System.out.println(caminhao);
-                }
+//        List<int[]> conjuntoDeTeste = GeradorDeProblemas.geracaoDeRotas(10, 10, 1);
+        List<int[]> conjuntoDeTeste = List.of(
+                new int[]{40,36,38,29,32,28,31,35,31,30,32,30,29,39,35,38,39,35,32,38,32,33,29,33,29,39,28},
+                new int[]{32,51,32,43,42,30,42,51,43,51,29,25,27,32,29,55,43,29,32,44,55,29,53,30,24,27});
+
+        for(int i = 0; i < conjuntoDeTeste.size(); i++) {
+            long startTime2 = System.currentTimeMillis();
+            System.out.println("------------" + "Conjunto de teste " + (i + 1) + "------------" );
+
+            int[] rotas = conjuntoDeTeste.get(i);
+            int meta = 0;
+
+            for (int rota : rotas) {
+                meta += rota;
             }
+            List<List<Number>> caminhoes = backtracking(rotas, new int[3], new ArrayList<>(), meta / 3, 0);
+
+            int[] totalParaRotas = new int[3];
+
+            for(int j = 0; j < caminhoes.size(); j++) {
+                System.out.println("Caminhão " + (j + 1) + ": " + caminhoes.get(j));
+                totalParaRotas[j] = caminhoes.get(j).stream().mapToInt(Number::intValue).sum();
+            }
+
+            System.out.println("Total para rotas: " + Arrays.toString(totalParaRotas));
+            System.out.println("Desvio padrão: " + calcularDesvioPadrao(totalParaRotas));
+            System.out.println("Amplitude: " + calcularAmplitude(totalParaRotas));
+
+
+            long endTime2 = System.currentTimeMillis();
+
+            long duration2 = endTime2 - startTime2;
+            double durationInSeconds2 = duration2 / 1000.0;
+            System.out.println("Tempo de execução atual: " + durationInSeconds2 + " segundos");
+
         }
 
+        System.out.println("------------" + "Fim dos testes" + "------------");
         long endTime = System.currentTimeMillis();
 
         long duration = endTime - startTime;
         double durationInSeconds = duration / 1000.0;
-        System.out.println("Tempo de execução: " + durationInSeconds + " segundos");
+        System.out.println("Tempo de execução geral: " + durationInSeconds + " segundos");
     }
 
 
@@ -67,7 +91,7 @@ class Backtraking {
         for (List<Number> caminhao : solucoes) {
             int kilometragemCaminhao = caminhao.stream().mapToInt(Number::intValue).sum();
 
-            if (Math.abs(kilometragemCaminhao - meta) > 10) {
+            if (Math.abs(kilometragemCaminhao - meta) >= 1) {
                 return false;
             }
         }
@@ -75,4 +99,44 @@ class Backtraking {
         return true;
     }
 
+    public static double calcularDesvioPadrao(int[] array) {
+        double soma = 0;
+        for (int valor : array) {
+            soma += valor;
+        }
+        double media = soma / array.length;
+
+        double somaQuadradosDiferencas = 0;
+        for (int valor : array) {
+            double diferenca = valor - media;
+            somaQuadradosDiferencas += diferenca * diferenca;
+        }
+
+        double variancia = somaQuadradosDiferencas / array.length;
+
+
+        return Math.sqrt(variancia);
+    }
+
+    public static int calcularAmplitude(int[] array) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException("O array não pode estar vazio.");
+        }
+
+        int menorValor = array[0];
+        int maiorValor = array[0];
+
+        for (int valor : array) {
+            if (valor < menorValor) {
+                menorValor = valor;
+            } else if (valor > maiorValor) {
+                maiorValor = valor;
+            }
+        }
+
+        return maiorValor - menorValor;
+    }
+
 }
+
+
